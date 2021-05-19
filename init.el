ea23248259b16bb02c6b5c265181f4d1c775b839
caixiangyue <caixiangyue007@gmail.com>
2021-05-19 14:02:48 +0800

    clean code

--- init.el
+++ init.el
@@ -9,12 +9,8 @@
   (gc-cons-threshold most-positive-fixnum)
   ;; 清空避免加载远程文件的时候分析文件。
   (file-name-handler-alist nil))
-
   (add-subdirs-to-load-path "~/.emacs.d/lisp/")
   )
-
-
-(setq inhibit-startup-message t)
 
 (require 'package)
 (setq package-enable-at-startup nil)
